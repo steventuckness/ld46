ec05671828b8bc1b4104ccb12d794394b4a99718
Daniel Barton <91.dan.barton@gmail.com>
2020-04-18 10:17:17 -0500

    Make 'Play Game' launch main scene
    Makes the Play Game button function.
    Also renames the QuitButton class to MenuActions to better represent
    it's purpose

--- ld46/Assets/Behaviors/MenuActions.cs
+++ ld46/Assets/Behaviors/MenuActions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class QuitButton : MonoBehaviour
+public class MenuActions : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
