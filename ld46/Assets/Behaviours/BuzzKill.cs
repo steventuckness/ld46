77b44402491b00385ab3ef3c012795ef10884f97
Steven Tuckness <stuckne1@gmail.com>
2020-04-18 11:22:23 -0500

    initial buzzkill system

---
+++ ld46/Assets/Behaviours/BuzzKill.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuzzKill : MonoBehaviour
+{
+    public float buzzKillFreeDuration = 60.0f;
+    public int buzzKillRangeMax = 20000;
+    public GameObject metrics;
+    public string[] buzzKills = { "message", "cost" };
+
+    private float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= buzzKillFreeDuration)  
+        {
+            var buzzKill = Random.Range(buzzKillFreeDuration, buzzKillRangeMax);
+            if (buzzKill <= timer) 
+            {
+                var buzzKillIndex = Random.Range(0, buzzKills.Length);
+                Debug.Log("buzzkill:" + buzzKills[buzzKillIndex]);
+            }
+            
+        }     
+    }
+}
