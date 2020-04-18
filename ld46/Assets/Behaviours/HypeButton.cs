344b857152f1175e78f713a7cf43001c9ca5b304
Leonardo Sa <leo@leo-sa.com>
2020-04-18 00:36:40 -0500

    Hype button

---
+++ ld46/Assets/Behaviours/HypeButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperButton : MonoBehaviour
+{
+    public float fatigueIncreasePerActivation;
+    public float currentFatigue;
+    public float fatigueDecreaseSpeed;
+    public int hypeFactor;
+    public int cost;
+    public string trackName;
+
+    public GameObject metrics;
+    public GameObject soundSystem;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (currentFatigue > 0) {
+            currentFatigue = currentFatigue - (fatigueDecreaseSpeed / Time.deltaTime);
+        }
+    }
+
+    void ActivateHype()
+    {
+        if (m.money - cost <= 0) { return; }
+        var m = metrics.GetComponent<Metrics>(); 
+        m.money -= cost; 
+        m.hype += hypeFactor - currentFatigue;
+        currentFatigue += fatigueIncreasePerActivation;
+        if (trackName != "") {
+            var s = soundSystem.GetComponent<Sound>();
+            s.UnmuteTrack(trackName);
+        }
+    }
+}
